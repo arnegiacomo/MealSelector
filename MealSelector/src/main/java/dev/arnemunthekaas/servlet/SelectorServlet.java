a49514726c92028a440d674b3981edc269d0b4ce
Arne Munthe-Kaas <45269747+h593267@users.noreply.github.com>
2022-01-27 11:00:23 +0100

    test

--- MealSelector/src/main/java/dev/arnemunthekaas/servlet/SelectorServlet.java
+++ MealSelector/src/main/java/dev/arnemunthekaas/servlet/SelectorServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dev.arnemunthekaas.DB.DAO.MealrelationsDAO;
-import dev.arnemunthekaas.mealview.MealView;
 import dev.arnemunthekaas.selector.MealSelector;
 import dev.arnemunthekaas.selector.SelectorForm;
 
